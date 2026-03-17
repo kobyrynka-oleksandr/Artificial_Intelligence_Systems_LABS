14c4245cb265e6abea91037ba51e6a7abdd8d4a4
Oleksandr_Kobyrynka <kobyrynka.oleksandr1123@vu.cdu.edu.ua>
2026-03-17 02:48:55 +0200

    Lab1 added

---
+++ L1/1_1_12/_1_1_12.py
@@ -0,0 +1,44 @@
+﻿import math
+
+try:
+    x = float(input("Введіть x (у градусах): "))
+
+    if x < 3:
+        print("Використовується функція: lg(cos(2x)/(x-0.5))")
+
+        denominator = x - 0.5
+        if denominator == 0:
+            print("Помилка: знаменник (x - 0.5) дорівнює нулю.")
+        else:
+            cos_value = math.cos(math.radians(2 * x))
+            value = cos_value / denominator
+
+            if value <= 0:
+                print("Помилка: аргумент логарифма ≤ 0.")
+            else:
+                y = math.log10(value)
+                print("y =", y)
+
+    elif x >= 4:
+        print("Використовується функція: ln(cos(x)/(1+tg(x^2)))")
+
+        cosx = math.cos(math.radians(x))
+        tanx2 = math.tan(math.radians(x ** 2))
+
+        denominator = 1 + tanx2
+        if denominator == 0:
+            print("Помилка: знаменник (1 + tg(x²)) дорівнює нулю.")
+        else:
+            value = cosx / denominator
+
+            if value <= 0:
+                print("Помилка: аргумент ln ≤ 0.")
+            else:
+                y = math.log(value)
+                print("y =", y)
+
+    else:
+        print("Функція не визначена при 3 ≤ x < 4.")
+
+except ValueError:
+    print("Помилка: введено не число.")
---
+++ L1/1_3_12/_1_3_12.py
@@ -0,0 +1,72 @@
+﻿import math
+
+def term(x, n, k):
+    try:
+        numerator = x ** (2*k + 1)
+        a = n - k
+
+        base = x + ((-1)**k) * (k + 1)
+        power = ((-1)**k) * (k + 1)
+
+        b = base ** power
+
+        denominator = a * b
+
+        if denominator == 0:
+            return False, 0
+
+        value = ((-1)**k) * numerator / denominator
+        return True, value
+    except:
+        return False, 0
+
+
+def calc_for(x, n):
+    s = 0
+    for k in range(n):
+        ok, t = term(x, n, k)
+        if not ok:
+            return False, 0
+        s += t
+    return True, s
+
+
+def calc_while(x, n):
+    s = 0
+    k = 0
+    while k < n:
+        ok, t = term(x, n, k)
+        if not ok:
+            return False, 0
+        s += t
+        k += 1
+    return True, s
+
+
+def calc_rec(x, n, k=0):
+    if k == n:
+        return True, 0
+
+    ok, t = term(x, n, k)
+    if not ok:
+        return False, 0
+
+    ok2, s = calc_rec(x, n, k+1)
+    if not ok2:
+        return False, 0
+
+    return True, t + s
+
+
+# main
+x = float(input("Введіть x: "))
+n = int(input("Введіть n: "))
+
+ok, res = calc_for(x, n)
+print("FOR:", res if ok else "Помилка обчислення")
+
+ok, res = calc_while(x, n)
+print("WHILE:", res if ok else "Помилка обчислення")
+
+ok, res = calc_rec(x, n)
+print("RECURSION:", res if ok else "Помилка обчислення")
